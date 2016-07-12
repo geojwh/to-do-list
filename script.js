6dc331c899cda1d0bdfd82d4b145107c38631f1b
geojwh <geojwh@gmail.com>
2016-07-12 14:31:50 -0500

    upload content

---
+++ script.js
@@ -0,0 +1,24 @@
+// Code goes here
+
+$('ul').on('click', 'li', function(){
+  $(this).toggleClass('completed');
+});
+
+$('ul').on('click', 'span', function(event){
+  $(this).parent().remove();
+});
+
+$('input').keypress(function(event){
+  if(event.which === 13){
+    var todoItem = $(this).val();
+    $('ul').append(
+    "<li>" +
+        "<span>" +
+          "X" +
+        "</span>" +
+        todoItem +
+    "</li>"
+    );
+    $(this).val('');
+  }
+});
